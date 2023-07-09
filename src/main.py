64e7061c3a72cdd5c575853cb1440da1289ad853
darylmathison <daryl.mathison@gmail.com>
2023-07-09 10:34:27 -0400

    Fixed the testing issue.

--- src/main.py
+++ src/main.py
@@ -1,24 +1,50 @@
 import pickle
+
+import business_calendar
 import numpy as np
 import yfinance
 import logging
+from business_calendar import MO, TU, WE, TH, FR
+import datetime
 
 logger = logging.getLogger(__name__)
 
 
 def percent_difference(value1, value2):
     numerator = abs(value1 - value2)
-    denominator = (value1 + value2)/2
+    denominator = value2
     return numerator/denominator * 100
 
 
-def test_model(stock_symbol):
+def date_range():
+    calendar = business_calendar.Calendar(workdays=[MO, TU, WE, TH, FR])
+    end_day = datetime.datetime.today()
+    day_delta = datetime.timedelta(days=1)
+
+    while not calendar.isworkday(end_day):
+        end_day = end_day - day_delta
+
+    return (end_day - day_delta), end_day
+
+
+def find_two_prior_closes(stock_symbol):
     ticker = yfinance.Ticker(stock_symbol)
-    predicted_close = get_predicted_value(ticker.info['previousClose'])
-    actual_close = float(ticker.info['close'])
+    start_day, end_day = date_range()
+    history = ticker.history(start=start_day, end=end_day)
+    print(history.info)
+    close = history.filter(["Close"])
+    return close.to_numpy()
+
+
+def test_model(stock_symbol):
+    closes = find_two_prior_closes(stock_symbol)
+    print(closes)
+
+    predicted_close = get_predicted_value(closes[0])
+    actual_close = float(closes[1])
     percent_diff = percent_difference(predicted_close, actual_close)
     logger.info("predicted close: %s; actual close: %s; percent difference %s",
-                predicted_close, ticker.info["close"], percent_diff)
+                predicted_close, actual_close, percent_diff)
 
 
 def get_predicted_value(input_value):
