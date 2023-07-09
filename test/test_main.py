73e70f86105dd4d1b43d2d3104ef544ea5657608
darylmathison <daryl.mathison@gmail.com>
2023-07-09 13:20:27 -0400

    Returning a dict

--- test/test_main.py
+++ test/test_main.py
@@ -1,5 +1,6 @@
 import unittest
 from unittest.mock import patch, MagicMock
+import pandas as pd
 import main
 
 
@@ -12,20 +13,19 @@ class TestMain(unittest.TestCase):
         actual = main.get_predicted_value("50.0")
         self.assertIsNotNone(actual)
 
-    @patch("main.find_two_prior_closes")
+    @patch("main.find_two_prior_closes", return_value=[12.0, 20.0])
     def test_test_model(self, two_closes):
-        two_closes.__call__ = MagicMock(return_value=[12.0, 20.0])
         main.test_model("CANE")
 
     @patch("main.yfinance.Ticker")
     def test_handle_signal_test_model(self, ticker):
-        ticker().info = {'previousClose': 21.0}
+        ticker().history = MagicMock(return_value=pd.DataFrame({"Close": [21.0, 20.0]}, index=[0, 1]))
         actual = main.handle(dict(), None)
-        self.assertEqual("OK", actual)
+        self.assertTrue(isinstance(actual, dict))
 
     @patch("main.yfinance.Ticker")
     def test_handle_signal_normal(self, ticker):
-        ticker().info = {"close": 20.0, 'previousClose': 21.0}
+        ticker().info = {'previousClose': 21.0}
         actual = main.handle({'yesterday_close': 15.0}, None)
         self.assertNotEqual("OK", actual)
 
